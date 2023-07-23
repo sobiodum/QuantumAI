ee0d76cec8dbc5a416763136f2a6a3b86ab5f78a
sobiodum <78501745+sobiodum@users.noreply.github.com>
2023-07-23 23:52:12 +0200

    firsttest2

---
+++ preprocessors/data_processor.py
@@ -0,0 +1,182 @@
+from __future__ import annotations
+
+import numpy as np
+import pandas as pd
+
+from data_processors.processor_alpaca import AlpacaProcessor as Alpaca
+from data_processors.processor_wrds import WrdsProcessor as Wrds
+from data_processors.processor_yahoofinance import (
+    YahooFinanceProcessor as YahooFinance,
+)
+
+
+class DataProcessor:
+    def __init__(self, data_source, **kwargs):
+        if data_source == "alpaca":
+            try:
+                API_KEY = kwargs.get("API_KEY")
+                API_SECRET = kwargs.get("API_SECRET")
+                API_BASE_URL = kwargs.get("API_BASE_URL")
+                self.processor = Alpaca(API_KEY, API_SECRET, API_BASE_URL)
+                print("Alpaca successfully connected")
+            except BaseException:
+                raise ValueError("Please input correct account info for alpaca!")
+
+        elif data_source == "wrds":
+            self.processor = Wrds()
+
+        elif data_source == "yahoofinance":
+            self.processor = YahooFinance()
+
+        else:
+            raise ValueError("Data source input is NOT supported yet.")
+
+    def download_data(
+        self, ticker_list, start_date, end_date, time_interval
+    ) -> pd.DataFrame:
+        df = self.processor.download_data(
+            ticker_list=ticker_list,
+            start_date=start_date,
+            end_date=end_date,
+            time_interval=time_interval,
+        )
+        return df
+
+    def clean_data(self, df) -> pd.DataFrame:
+        df = self.processor.clean_data(df)
+
+        return df
+
+    def add_technical_indicator(self, df, tech_indicator_list) -> pd.DataFrame:
+        self.tech_indicator_list = tech_indicator_list
+        df = self.processor.add_technical_indicator(df, tech_indicator_list)
+
+        return df
+
+    def add_turbulence(self, df) -> pd.DataFrame:
+        df = self.processor.add_turbulence(df)
+
+        return df
+
+    def add_vix(self, df) -> pd.DataFrame:
+        df = self.processor.add_vix(df)
+
+        return df
+
+    def df_to_array(self, df, if_vix) -> np.array:
+        price_array, tech_array, turbulence_array = self.processor.df_to_array(
+            df, self.tech_indicator_list, if_vix
+        )
+        # fill nan and inf values with 0 for technical indicators
+        tech_nan_positions = np.isnan(tech_array)
+        tech_array[tech_nan_positions] = 0
+        tech_inf_positions = np.isinf(tech_array)
+        tech_array[tech_inf_positions] = 0
+
+        return price_array, tech_array, turbulence_array
+
+class FloDataProcessor:
+    def __init__(self, df):
+        self.df = df
+
+
+
+    def clean_data(self, df: pd.DataFrame) -> pd.DataFrame:
+        df['timestamp'] = df['timestamp'].dt.tz_localize(None)
+        tic_list = np.unique(df.tic.values)
+        NY = "America/New_York"
+
+        trading_days = self.get_trading_days(start=self.start, end=self.end)
+        # produce full timestamp index
+        if self.time_interval == "1d":
+            times = trading_days
+        elif self.time_interval == "1m":
+            times = []
+            for day in trading_days:
+                #                NY = "America/New_York"
+                current_time = pd.Timestamp(day + " 09:30:00").tz_localize(NY)
+                #!THis to try fix error
+                # current_time = pd.Timestamp(day + " 09:30:00").tz_localize(None)
+                for i in range(390):  # 390 minutes in trading day
+                    times.append(current_time)
+                    current_time += pd.Timedelta(minutes=1)
+        else:
+            raise ValueError(
+                "Data clean at given time interval is not supported for YahooFinance data."
+            )
+
+        # create a new dataframe with full timestamp series
+        new_df = pd.DataFrame()
+        for tic in tic_list:
+            tmp_df = pd.DataFrame(
+                columns=["open", "high", "low", "close", "volume"], index=times
+            )
+            tic_df = df[
+                df.tic == tic
+            ]  # extract just the rows from downloaded data relating to this tic
+            for i in range(tic_df.shape[0]):  # fill empty DataFrame using original data
+                tmp_df.loc[tic_df.iloc[i]["timestamp"].tz_localize(NY)] = tic_df.iloc[
+                    i
+                ][["open", "high", "low", "close", "volume"]]
+            # print("(9) tmp_df\n", tmp_df.to_string()) # print ALL dataframe to check for missing rows from download
+
+            # if close on start date is NaN, fill data with first valid close
+            # and set volume to 0.
+            if str(tmp_df.iloc[0]["close"]) == "nan":
+                print("NaN data on start date, fill using first valid data.")
+                for i in range(tmp_df.shape[0]):
+                    if str(tmp_df.iloc[i]["close"]) != "nan":
+                        first_valid_close = tmp_df.iloc[i]["close"]
+                        tmp_df.iloc[0] = [
+                            first_valid_close,
+                            first_valid_close,
+                            first_valid_close,
+                            first_valid_close,
+                            0.0,
+                        ]
+                        break
+
+            # if the close price of the first row is still NaN (All the prices are NaN in this case)
+            if str(tmp_df.iloc[0]["close"]) == "nan":
+                print(
+                    "Missing data for ticker: ",
+                    tic,
+                    " . The prices are all NaN. Fill with 0.",
+                )
+                tmp_df.iloc[0] = [
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                ]
+
+            # fill NaN data with previous close and set volume to 0.
+            for i in range(tmp_df.shape[0]):
+                if str(tmp_df.iloc[i]["close"]) == "nan":
+                    previous_close = tmp_df.iloc[i - 1]["close"]
+                    if str(previous_close) == "nan":
+                        raise ValueError
+                    tmp_df.iloc[i] = [
+                        previous_close,
+                        previous_close,
+                        previous_close,
+                        previous_close,
+                        0.0,
+                    ]
+                    # print(tmp_df.iloc[i], " Filled NaN data with previous close and set volume to 0. ticker: ", tic)
+
+            # merge single ticker data to new DataFrame
+            tmp_df = tmp_df.astype(float)
+            tmp_df["tic"] = tic
+            new_df = pd.concat([new_df, tmp_df])
+
+        #            print(("Data clean for ") + tic + (" is finished."))
+
+        # reset index and rename columns
+        new_df = new_df.reset_index()
+        new_df = new_df.rename(columns={"index": "timestamp"})
+
+        #        print("Data clean all finished!")
+
+        return new_df
